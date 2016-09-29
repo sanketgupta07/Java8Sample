d900a53dea08284bfc9dc48f714923bdccfcd597
Sanket Gupta <sanketgupta07@gmail.com>
2016-09-30 00:23:57 +0530

    optimized (ctrl+o)

--- src/com/java8in/action/ch5/BuildingStream.java
+++ src/com/java8in/action/ch5/BuildingStream.java
@@ -3,11 +3,9 @@
  */
 package com.java8in.action.ch5;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.stream.Stream;
